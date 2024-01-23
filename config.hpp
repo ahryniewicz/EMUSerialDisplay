07fb5acddc15e0c2ff41612a525a804a9281b60b
ahryniewicz <ahryniewicz@taoglas.com>
2024-01-23 21:39:42 +0100

    Add bat cel and egt in config

--- config.hpp
+++ config.hpp
@@ -4,8 +4,11 @@
 #define USE_BUZZER_FOR_ALARM            0
 
 #define USE_IAT_PARAMETER               1
+#define USE_EGT_PARAMETER               1
+#define USE_BATT_PARAMETER              1
 #define USE_LAMBDA_PARAMETER            1
 #define USE_LAMBDA_TARGET_PARAMETER     1
+#define USE_CEL_PARAMETER               1
 #define USE_AFR_TARGET_PARAMETER        0
 #define USE_KNOCK_LEVEL_PARAMETER       0
 #define USE_RPM_PARAMETER               0
